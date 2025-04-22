12dfa71a76a7de03c76b26ac6de29c71c092538e
Sergio Martinez Sahuquillo <smartsah@mercadona.es>
2025-04-22 16:35:26 +0200

    Subida inicial del baackend Springboot y la estructura del android-app

---
+++ android-app/app/src/main/java/com/example/mybaskettrainer/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.mybaskettrainer.navigation
+
+sealed class Routes(val route: String) {
+    object MainScreen : Routes("main_screen")
+    object SplashScreen : Routes("splash_screen")
+}
